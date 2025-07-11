2505f9fd5b173c44556dfc7fa5272bd4afafbf59
KZH-etu <kessezoh@gmail.com>
2025-07-11 11:20:44 +0000

    Add NotePreview and NotesPreviewList components, mock data, and utility functions for date formatting

---
+++ src/renderer/src/store/mocks/index.ts
@@ -0,0 +1,21 @@
+import { NoteInfo } from "@shared/models";
+
+
+export const notesMock: NoteInfo[] = [
+    {
+        title: `Welcome to NoteMark Z`,
+        lastEditTime: Date.now()
+    },
+    {
+        title: `Note 1`,
+        lastEditTime: Date.now()
+    },
+    {
+        title: `Note 2`,
+        lastEditTime: Date.now()
+    },
+    {
+        title: `Note 3`,
+        lastEditTime: Date.now()
+    }
+]
--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -1,6 +1,14 @@
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+export const dateFormatter = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'short',
+    timeStyle: 'short',
+    timeZone: 'UTC',
+});
+
+export const formatDateFromTimestamp = (timestamp: number) => dateFormatter.format(timestamp);
+
 export const cn = (...args: ClassValue[]) => {
     return twMerge(clsx(args))
 }
